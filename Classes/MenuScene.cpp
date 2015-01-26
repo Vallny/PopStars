ed9e8cae215a1ed24632ec07e59e82b968b890c4
SIA <cveranm@gmail.com>
2015-01-26 23:19:42 +0800

    跳转到GameLayer

--- Classes/MenuScene.cpp
+++ Classes/MenuScene.cpp
@@ -1,12 +1,20 @@
 #include "MenuScene.h"
 #include "MenuLayer.h"
 #include "Audio.h"
+#include "GameData.h"
 using namespace CocosDenshion;
+
+
+
 bool MenuScene::init(){
 	if(!Scene::init()){
 		return false;
 	}
 	//Audio::getInstance()->playBGM();
 	this->addChild(MenuLayer::create());
+
+
+
+
 	return true;
 }
